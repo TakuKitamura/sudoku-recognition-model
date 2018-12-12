3985bd65f1eb1ea4f57e19e970aeda42779798b4
TakuKitamura <tikutaku0128@icloud.com>
2018-12-13 00:32:19 +0900

    can get corner points

--- src/py/pyPlaygorund.py
+++ src/py/pyPlaygorund.py
@@ -21,7 +21,7 @@ def opening(img: np.ndarray, kernelShape: tuple) -> np.ndarray:
     kernel = np.ones(kernelShape,np.uint8)
     return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-file_path = '3.jpg'
+file_path = '2.jpg'
 img_org = cv2.imread(file_path)
 
 img = cv2.imread(file_path, 0)
@@ -30,7 +30,10 @@ gray_org_img = img
 
 height, width = img.shape[:2]
 
-i = 10
+if height > 10000 or width > 10000:
+    exit(1)
+
+i = 5
 
 while i > 0:
     img = gray_org_img
@@ -59,19 +62,89 @@ while i > 0:
     maxArea = height * width
     
     for cnt in contours:
+        # print(max(np.squeeze(cnt)[0]))
+
+        xMaxList = []
+        xMinList = []
+        yMaxList = []
+        yMinList = []
+        # for v in np.squeeze(cnt):
+        #     if (np.array([0, 0]) == v).all():
+        #         print("ok1")
+        #     elif (np.array([height - 1, width - 1]) == v).all():
+        #         print("ok2")
+
+        # print(cnt[:, 0])
+        # xMaxList.append(max(cnt[:, 0]))
+        # xMinList.append(min(cnt[:, 0]))
+
+        # yMaxList.append(max(cnt[:, 1]))
+        # yMinList.append(min(cnt[:, 1]))
+        sqeeze = np.squeeze(cnt)
+        # print(min(a[:, 0]))
+        # print(max(a[:, 0]))
+        # print(min(a[:, 1]))
+        # print(max(a[:, 1]))
+
+        xMin = min(sqeeze[:, 0])
+        xMax = max(sqeeze[:, 0])
+        yMin = min(sqeeze[:, 1])
+        yMax = max(sqeeze[:, 1])
+
+        if xMin == 0 or xMax == width - 1 or yMin == 0 or yMax == height -1:
+            # print(123)
+            # print(cnt)
+            continue
+
         area = cv2.contourArea(cnt)
         perimeter = cv2.arcLength(cnt,True)
-        print(area, perimeter)
-        if (area > maxArea * 0.3) and (maxArea * 0.85 > area):
-            epsilon = 0.01 * cv2.arcLength(cnt,True)
-            approx = cv2.approxPolyDP(cnt,epsilon,True)
-            cv2.drawContours(img_org,[approx],-1,[0,255,0],2)
+        k = cv2.isContourConvex(cnt)
+        # print(area, perimeter, k)
+        # print(i)
+        # if (area > maxArea * 0.3) and (maxArea * 0.95 > area):
+        if area >= maxArea/2 and area <= maxArea:
+            # epsilon = 0.01 * perimeter
+            # approx = cv2.approxPolyDP(cnt,epsilon,True)
+            # cv2.drawContours(img_org,[approx],-1,[0,255,0],2)
             i = 0
+            # print(xMin, xMax, yMin, yMax)
+            p1=[-1, -1]
+            p2=[-1, -1]
+            p3=[-1, -1]
+            p4=[-1, -1]
+            firstIn = -1
+            for v in np.squeeze(cnt):
+                # print(v)
+                if v[0] == xMin:
+                    p1[0], p1[1] = v
+                    firstIn = 1
+                    print(1)
+                elif v[1] == yMin:
+                    p2[0], p2[1] = v
+                    print(2)
+                elif v[1] == yMax:
+                    p3[0], p3[1] = v
+                    print(3)
+                elif v[0] == xMax:
+                    p4[0], p4[1] = v
+                    firstIn = 4
+                    print(4)
+            print(p1, p2, p3, p4)
+            pts2 = np.float32([[0,0],[height,0],[0,width],[height,width]])
+            # if firstIn == 
+            #     pass
+            # if xMin == p2[0]:
+            pts1 = np.float32([p1,p2,p3,p4])
+            # else:
+            #     pts1 = np.float32([p2,p4,p1,p3])
+
+            M = cv2.getPerspectiveTransform(pts1,pts2)
+            rst = cv2.warpPerspective(img_org,M,(height,width))
             break
     i -= 1
 
 cv2.imwrite('original.jpg',img_org)
-cv2.imwrite('o.jpg',th3)
+cv2.imwrite('o.jpg',rst)
 
 # height, width = img.shape[:2]
 # print(width, height)
