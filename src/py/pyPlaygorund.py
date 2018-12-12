faa8340ece8855bdf0136c6a1396d6798d8fd59b
TakuKitamura <tikutaku0128@icloud.com>
2018-12-12 18:04:09 +0900

    modify

--- src/py/pyPlaygorund.py
+++ src/py/pyPlaygorund.py
@@ -2,95 +2,217 @@ import cv2
 import numpy as np
 
 
-filter = True
+
+# filter = True
 
 
-file_path = 'a.png'
-img = cv2.imread(file_path)
+def erode(img: np.ndarray, kernelShape: tuple) -> np.ndarray:
+    kernel = np.ones(kernelShape,np.uint8)
+    return cv2.erode(img, kernel, iterations = 1)
 
-# グレースケールに変換
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+def dilate(img: np.ndarray, kernelShape: tuple) -> np.ndarray:
+    kernel = np.ones(kernelShape,np.uint8)
+    return cv2.dilate(img, kernel, iterations = 1)
 
-# キャニー法によるエッジ検出
-edges = cv2.Canny(gray,90,150,apertureSize = 3)
+def gaussianblur(img: np.ndarray, ksize: tuple) -> np.ndarray:
+    return cv2.GaussianBlur(img, ksize, 0)
+
+file_path = 'a.jpg'
+img_org = cv2.imread(file_path)
+
+img = cv2.imread(file_path, 0)
+
+blur = cv2.GaussianBlur(img,(5,5),0)
+ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+#find the countours 
+# imgEdge, contours0,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+# #size of the image (height, width)
+# h, w = img.shape[:2]
+
+# #copy the original image to show the posible candidate
+# image_sudoku_candidates = img.copy()
+
+# size_rectangle_max = 0; 
+# for i in range(len(contours0)):
+#     #aproximate countours to polygons
+#     approximation = cv2.approxPolyDP(contours0[i], 4, True)
+        
+#     #has the polygon 4 sides?
+#     if(not (len (approximation)==4)):
+#         continue;
+#     #is the polygon convex ?
+#     if(not cv2.isContourConvex(approximation) ):
+#         continue; 
+#     #area of the polygon
+#     size_rectangle = cv2.contourArea(approximation)
+#     #store the biggest
+#     if size_rectangle> size_rectangle_max:
+#         size_rectangle_max = size_rectangle 
+#         big_rectangle = approximation
+
+# approximation = big_rectangle
+# for i in range(len(approximation)):
+#     cv2.line(image_sudoku_candidates,
+#              (big_rectangle[(i%4)][0][0], big_rectangle[(i%4)][0][1]), 
+#              (big_rectangle[((i+1)%4)][0][0], big_rectangle[((i+1)%4)][0][1]),
+#              (255, 0, 0), 2)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# img = erode(img, (100, 100))
+
+# edges = cv2.Canny(img,50,150,apertureSize = 3)
+
+# lines = cv2.HoughLines(edges,1,np.pi/180,200)
+# for rho,theta in lines[0]:
+#     a = np.cos(theta)
+#     b = np.sin(theta)
+#     x0 = a*rho
+#     y0 = b*rho
+#     x1 = int(x0 + 1000*(-b))
+#     y1 = int(y0 + 1000*(a))
+#     x2 = int(x0 - 1000*(-b))
+#     y2 = int(y0 - 1000*(a))
+
+# cv2.line(img_org,(x1,y1),(x2,y2),(0,0,255),2)
+
+# img = dilate(img, (100, 100))
+
+# ret, thresh = cv2.threshold(img, 250, 256, cv2.THRESH_TOZERO_INV) 
+
+imgEdge,contours,hierarchy = cv2.findContours(th3, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+contours.sort(key=cv2.contourArea, reverse=True)
+
+for cnt in contours:
+    epsilon = 0.005 * cv2.arcLength(cnt,True)
+    approx = cv2.approxPolyDP(cnt,epsilon,True)
+    cv2.drawContours(img_org,[approx],-1,[0,255,0],2)    # 等高線の太さ
+    # print(cnt)
+
+cv2.imwrite('original.jpg',img_org)
+cv2.imwrite('o.jpg',th3)
+
+# height, width = img.shape[:2]
+# print(width, height)
+
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# gray_org = img
+
+# i = 30
+# maxArea = height * width
+# while i > 0:
+#     img = gray_org
+#     # img = dilate(img, (i, i))
+#     img = erode(img, (3, 3))
+#     # img = gaussianblur(img, (3,3))
+#     print(i)
+#     cv2.imwrite(str(i) + 'o.jpg',img)
+
+#     ret, thresh = cv2.threshold(img, 250, 256, cv2.THRESH_TOZERO_INV) 
+
+#     imgEdge,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+#     contours.sort(key=cv2.contourArea, reverse=True)
+#     for cnt in contours:
+#         area = cv2.contourArea(cnt)
+#         if area < maxArea * 0.3:
+#             continue
+
+#         if  maxArea * 0.95 < area:
+#             continue
+#         epsilon = 0.005*cv2.arcLength(cnt,True)
+#         approx = cv2.approxPolyDP(cnt,epsilon,True)
+#         cv2.drawContours(img_org,[approx],-1,[0,255,0],2)    # 等高線の太さ
+#         i = 0
+#         # print(area)
+#         # break
+#     i-= 1
+# cv2.imwrite('o.jpg',img_org)
+# # グレースケールに変換
+# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# # キャニー法によるエッジ検出
+# edges = cv2.Canny(gray,90,150,apertureSize = 3)
 
 
-kernel = np.ones((3,3),np.uint8)
+# kernel = np.ones((3,3),np.uint8)
 
-# モルフォロジー膨張変換
-edges = cv2.dilate(edges,kernel,iterations = 1)
-kernel = np.ones((5,5),np.uint8)
+# # モルフォロジー膨張変換
+# edges = cv2.dilate(edges,kernel,iterations = 1)
+# kernel = np.ones((5,5),np.uint8)
 
-# モルフォロジー収縮変換
-edges = cv2.erode(edges,kernel,iterations = 1)
-cv2.imwrite('canny.jpg',edges)
+# # モルフォロジー収縮変換
+# edges = cv2.erode(edges,kernel,iterations = 1)
+# cv2.imwrite('canny.jpg',edges)
 
-# ハフ変換による直線検出
-lines = cv2.HoughLines(edges,1,np.pi/180,150)
+# # ハフ変換による直線検出
+# lines = cv2.HoughLines(edges,1,np.pi/180,150)
 
-if not lines.any():
-    print('No lines were found')
-    exit()
+# if not lines.any():
+#     print('No lines were found')
+#     exit()
 
-if filter:
-    rho_threshold = 15
-    theta_threshold = 0.1
+# if filter:
+#     rho_threshold = 15
+#     theta_threshold = 0.1
 
-    # how many lines are similar to a given one
-    similar_lines = {i : [] for i in range(len(lines))}
-    for i in range(len(lines)):
-        for j in range(len(lines)):
-            if i == j:
-                continue
+#     # how many lines are similar to a given one
+#     similar_lines = {i : [] for i in range(len(lines))}
+#     for i in range(len(lines)):
+#         for j in range(len(lines)):
+#             if i == j:
+#                 continue
 
-            rho_i,theta_i = lines[i][0]
-            rho_j,theta_j = lines[j][0]
-            if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
-                similar_lines[i].append(j)
+#             rho_i,theta_i = lines[i][0]
+#             rho_j,theta_j = lines[j][0]
+#             if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
+#                 similar_lines[i].append(j)
 
-    # ordering the INDECES of the lines by how many are similar to them
-    indices = [i for i in range(len(lines))]
-    indices.sort(key=lambda x : len(similar_lines[x]))
+#     # ordering the INDECES of the lines by how many are similar to them
+#     indices = [i for i in range(len(lines))]
+#     indices.sort(key=lambda x : len(similar_lines[x]))
 
-    # line flags is the base for the filtering
-    line_flags = len(lines)*[True]
-    for i in range(len(lines) - 1):
-        if not line_flags[indices[i]]: # if we already disregarded the ith element in the ordered list then we don't care (we will not delete anything based on it and we will never reconsider using this line again)
-            continue
+#     # line flags is the base for the filtering
+#     line_flags = len(lines)*[True]
+#     for i in range(len(lines) - 1):
+#         if not line_flags[indices[i]]: # if we already disregarded the ith element in the ordered list then we don't care (we will not delete anything based on it and we will never reconsider using this line again)
+#             continue
 
-        for j in range(i + 1, len(lines)): # we are only considering those elements that had less similar line
-            if not line_flags[indices[j]]: # and only if we have not disregarded them already
-                continue
+#         for j in range(i + 1, len(lines)): # we are only considering those elements that had less similar line
+#             if not line_flags[indices[j]]: # and only if we have not disregarded them already
+#                 continue
 
-            rho_i,theta_i = lines[indices[i]][0]
-            rho_j,theta_j = lines[indices[j]][0]
-            if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
-                line_flags[indices[j]] = False # if it is similar and have not been disregarded yet then drop it now
+#             rho_i,theta_i = lines[indices[i]][0]
+#             rho_j,theta_j = lines[indices[j]][0]
+#             if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
+#                 line_flags[indices[j]] = False # if it is similar and have not been disregarded yet then drop it now
 
-print('number of Hough lines:', len(lines))
+# print('number of Hough lines:', len(lines))
 
-filtered_lines = []
+# filtered_lines = []
 
-if filter:
-    for i in range(len(lines)): # filtering
-        if line_flags[i]:
-            filtered_lines.append(lines[i])
+# if filter:
+#     for i in range(len(lines)): # filtering
+#         if line_flags[i]:
+#             filtered_lines.append(lines[i])
 
-    print('Number of filtered lines:', len(filtered_lines))
-else:
-    filtered_lines = lines
+#     print('Number of filtered lines:', len(filtered_lines))
+# else:
+#     filtered_lines = lines
 
-for line in filtered_lines:
-    rho,theta = line[0]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
+# for line in filtered_lines:
+#     rho,theta = line[0]
+#     a = np.cos(theta)
+#     b = np.sin(theta)
+#     x0 = a*rho
+#     y0 = b*rho
+#     x1 = int(x0 + 1000*(-b))
+#     y1 = int(y0 + 1000*(a))
+#     x2 = int(x0 - 1000*(-b))
+#     y2 = int(y0 - 1000*(a))
 
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+#     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
-cv2.imwrite('hough.jpg',img)
+# cv2.imwrite('hough.jpg',img)
